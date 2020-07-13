83ab6795282a483ebc720032478b40ef81187cc5
Nikolay Kochetov <nikochetov@gmail.com>
2020-07-13 13:54:12 +0300

    update index.jsj

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Board from './gameComp/Board';
-import './index.css';
+import Game from './gameComp/Game';
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
 
 ReactDOM.render(
   <Game />,
